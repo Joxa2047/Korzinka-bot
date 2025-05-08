7713997539d6f79ffebd2eab2ec23f711ee0d990
Javohir Imomaliyev <163696126+Joxa2047@users.noreply.github.com>
2025-05-08 08:19:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,1 @@
-BOT_TOKEN = "7854511236:AAG-cVV6gH8gFBbTyYf7tyRiy6_u5MBctOQ"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "korzinka"
-DB_USER = "postgres"
-DB_PASSWORD = "2047"
 
-ADMIN_IDS = [
-    7888620964
-]
-CURRENCY = "so'm"
